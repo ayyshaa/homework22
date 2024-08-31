574643a509c7edcf6b7272afaa7ff3e6b5e3df1a
ayyshaa <qacarovaayse@gmail.com>
2024-08-31 19:09:28 +0400

    case

---
+++ app.js
@@ -0,0 +1,50 @@
+let month = prompt("Ayin nomresini daxil edin:");
+switch(month){
+    case "1":
+    case "01":
+        console.log("Bir");
+    break;
+    case "2":
+    case "02":
+        console.log("Iki");
+    break;
+    case "3":
+    case "03":
+        console.log("Uc");
+    break;
+    case "4":
+    case "04":
+        console.log("Dord");
+    break;
+    case "5":
+    case "05":
+        console.log("Bes");
+    break;
+    case "6":
+    case "06":
+        console.log("Alti");
+    break;
+    case "7":
+    case "07":
+        console.log("Yeddi");
+    break;
+    case "8":
+    case "08":
+        console.log("Sekkiz");
+    break;
+    case "9":
+    case "09":
+        console.log("Dokkuz");
+    break;
+    case "10":
+        console.log("On");
+    break;
+    case "11":
+        console.log("On bir");
+    break;
+    case "12":
+        console.log("On iki");
+    break;
+    default:
+        console.log("Bele ay yoxdur.");
+}
